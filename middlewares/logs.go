811c892dd6f8c8cd610769a9886004910c86e510
go-escape <goescape135@gmail.com>
2025-04-13 14:33:02 +0700

    chore: improvement logging

--- middlewares/logs.go
+++ middlewares/logs.go
@@ -8,14 +8,14 @@ import (
 
 func Logger(router *gin.Engine) gin.HandlerFunc {
 	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-		return fmt.Sprintf("[%s] | %d | %v | %s | %s %s | %s | %s\n",
+		latencyMs := float64(param.Latency.Nanoseconds()) / 1e6
+		return fmt.Sprintf("[%s] | %d | %.2fms | %s | %s %s | %s\n",
 			param.TimeStamp.Format("2006-01-02 15:04:05"),
 			param.StatusCode,
-			param.Latency,
+			latencyMs,
 			param.ClientIP,
 			param.Method, param.Path,
 			param.Request.UserAgent(),
-			param.ErrorMessage,
 		)
 	})
 }
